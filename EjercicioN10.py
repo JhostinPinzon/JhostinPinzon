041b725b7fd8274321d2d3802a5e3cbd36f5aa07
JhostinPinzon <jhostinpinzonbohorquez29@gmail.com>
2025-06-03 11:09:27 -0500

    Delete EjercicioN10.py

--- EjercicioN10.py
+++
@@ -1,4 +0,0 @@
-salario_base = float(input("Ingresa el salario base: "))
-bono = salario_base * 0.10
-salario_total = salario_base + bono
-print("El salario total con bonificación es:", salario_total)
