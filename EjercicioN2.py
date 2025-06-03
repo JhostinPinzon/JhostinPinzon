409987242bdecdf632ec033118ae2b368ac4317e
JhostinPinzon <jhostinpinzonbohorquez29@gmail.com>
2025-06-03 11:05:58 -0500

    Add files via upload

---
+++ EjercicioN2.py
@@ -0,0 +1,3 @@
+celsius = float(input("Ingresa los grados Celsius: "))
+fahrenheit = celsius * 1.8 + 32
+print("Grados Fahrenheit:", fahrenheit)
---
+++ EjercicioN5.py
@@ -0,0 +1,5 @@
+x = input("Ingresa el valor de x: ")
+y = input("Ingresa el valor de y: ")
+x, y = y, x
+print("Nuevo valor de x:", x)
+print("Nuevo valor de y:", y)
